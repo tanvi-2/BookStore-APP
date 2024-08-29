086d9e660bc202013122b33c2ed369952faa567a
tanvi_gupta_07 <tanvigupta836@gmail.com>
2024-08-29 22:01:31 +0530

    Done

--- Backend/index.js
+++ Backend/index.js
@@ -5,23 +5,24 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
+import path from "path";
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 
-app.use(cors());
+app.use(cors("http://localhost:5173/",));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 4001;
-//const URI = process.env.MONGO_URI;
+
+const URI = process.env.MONGO_URI;
 
 //console.log('MongoDB URI:', URI);
 
 // Connect to MongoDB
 
-const URI = "mongodb+srv://tanvigupta836:GXIF7Ni2pP7bJZHn@cluster0.iieny2z.mongodb.net/";
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log('MongoDB connection error:', err));
